4d000634dc44e6ba2437dd67d41351fe1e99f67e
TJWGMK5G <87062165+TJWGMK5G@users.noreply.github.com>
2023-06-17 17:03:29 +0300

    123
    123

--- gulp/tasks/reset.js
+++ gulp/tasks/reset.js
@@ -1,5 +1,11 @@
-import del from 'del';
+// import * as del from "del";
+
+// export const reset = () => {
+//   return del(app.path.clean);
+// }
+
+import {deleteAsync}  from "del";
 
 export const reset = () => {
-  return del(app.path.clean);
+    return deleteAsync(app.path.clean);
 }
