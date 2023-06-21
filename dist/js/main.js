dffacb7a39db1d24d43dc5ebbdb69d9a334428e6
TJWGMK5G <87062165+TJWGMK5G@users.noreply.github.com>
2023-06-21 19:52:01 +0300

    123
    213

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,30 +1,55 @@
-new Swiper('.main-swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-        },
-        autoHeight: true,
-        initialSlide: 0,
-        // slidesPerView: 1,
-        spaceBetween: 150,
-        loop: true,
-        slidesPerGroup: 1,
-        centeredSlides: true,
-        loop: true,
-        // autoplay: {
-        //     delay: 4000,
-        //     disavleOnInterection: true,
-        // },
-        speed: 1000,
-  });
+if (document.querySelector('.main-swiper') == true) {
+    new Swiper('.main-swiper', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true,
+            },
+            autoHeight: true,
+            initialSlide: 0,
+            // slidesPerView: 1,
+            spaceBetween: 150,
+            loop: true,
+            slidesPerGroup: 1,
+            centeredSlides: true,
+            loop: true,
+            // autoplay: {
+            //     delay: 4000,
+            //     disavleOnInterection: true,
+            // },
+            speed: 1000,
+      });
+}
+
 
 
 //   News Company
 const btn = document.querySelector('.main-news__btn');
-const item = document.querySelectorAll('.news-item');
+const item = document.querySelectorAll('.main-news-item');
+
+let flag = false;
 
 btn.addEventListener('click' , function() {
-    btn.innerHTML = "Скрыть"
+    flag = !flag
+    if (flag == true) {
+        btn.innerHTML = "Скрыть"
+    } else {
+        btn.innerHTML = 'Открыть'
+    }   
 })
 
+item.forEach(function (i) {
+       btn.addEventListener('click' , function () {
+        i.classList.toggle("active");
+       })
+})
+
+
+// Добавление бг цвет для всех последующий страниц, кроме главной 
+// Если класс personal на странице есть, то у хедера появляется background-color 
+// if (document.querySelector('.popup-personal') == true) {
+//     хедеру добавить цвет (создаём новый класс в котором указываем css стиль bg-color и навешиваем на хедер)
+//     document.querySelector('')
+// } else {
+//     (document.querySelector('.popup-personal') == false)
+// }
